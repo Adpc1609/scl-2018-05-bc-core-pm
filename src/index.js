bfe744b2da0d8052cf0e6b6045c5f40479ef922f
Adriana Perez <adpc1609@gmail.com>
2018-05-25 17:52:32 -0400

    ciclo for para cifrado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 
 function encode(){
+    //validacion de que se intoduca un numero en desplazamiento
+    //valida que el textarea no este vacio
     let numHash= document.getElementById("numHash").value;
     let texto= document.getElementById('termino').value; 
+    let codificado='';
+    let resultAscii;//guardar ascii de la letra
+    let newAscii; //codigo ascii por cada letra
         if(texto===''){ 
             console.log('el termino no puede ser vacio') 
             return;
@@ -13,9 +18,14 @@ function encode(){
 
         console.log('datos validos');
          let response =window.cipher.encode(numHash, texto);
- // console.log("event click", termino, numHash);
-
-    // document.getElementById("termino").value = "me vas a pagar con......";
+ console.log("event click", termino, numHash);
+ for (var i = 0; i<texto.length; i++) {
+    let texto= document.getElementById('termino').value; 
+     resultAscii= texto(i).charCodeAt();//obtenemos ascii;
+     newAscii= resultAscii+numHash;
+     codificado= codificado.concat(string.fromCharcode(newAscii));
+    }
+  //return salida;
 }
 
 function decode(){
@@ -32,7 +42,7 @@ function decode(){
 
         console.log('datos validos');
          let response =window.cipher.decode(numHash, texto);
- // console.log("event click", termino, numHash);
+      // console.log("event click", termino, numHash);
 
-    // document.getElementById("termino").value = "me vas a pagar con......";
+
 }
