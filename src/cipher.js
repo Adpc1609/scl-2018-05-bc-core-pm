fcc6a788dd8a484aa096ecaac0bfc2777cb4b6b4
Adriana Perez <adpc1609@gmail.com>
2018-05-24 22:04:11 -0400

    creacion de funciones en index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,8 @@
 window.cipher = {
   encode: (offset,string) => {
     /* los elementos que queremos desplazar a la derecha */
+
+    console.log('encode');
   },
   decode: (offset,string) => {
     /* los elementos que queremos desplazar a la izquierda */
