9a6d231ba1d3d8c0fbd4ae8c1265bffabb0c5ef6
Adriana Perez <adpc1609@gmail.com>
2018-05-30 22:54:21 -0400

    validaciones,boton clear,imprimir cifrado y descifrado

--- cipher.js
+++ cipher.js
@@ -1,4 +1,4 @@
-window.cipher = {
+ window.cipher = {
   encode: (offset,termino) => {
     /* los elementos que queremos desplazar a la derecha */
 
@@ -7,30 +7,40 @@ window.cipher = {
     for (var i = 0; i<termino.length; i++) {
        // texto a descifrar-convertir a Mayusculas/ obtener ascii en esa posicion
        resultAscii= termino[i].toUpperCase().charCodeAt();//obtenemos ascii;
-       newAscii = (resultAscii - 65 + Number(offset)) % 26 + 65;
-       resultnew = String.fromCharCode(newAscii);
-       newTermino+=resultnew;
+
+       if(resultAscii===32){
+        newTermino+=' ';
+      }else{
+        newAscii = (resultAscii - 65 + Number(offset)) % 26 + 65;//formula cifrado cesar
+        resultnew = String.fromCharCode(newAscii); //convierte el ascii sustituto en la nueva letra s
+        newTermino+=resultnew;
+      }
        
       }
       console.log(newTermino);
-    //return newTermino;
-    document.getElementById("termino").value="palabra cifrada: " + newTermino;
+    return newTermino;
+   
 
   },
+
   decode: (offset,termino) => {
     //console.log('decode');
     newTermino='';
     for (var i =0; i<termino.length; i++){
       resultAscii=termino[i].toUpperCase().charCodeAt();
-      console.log(resultAscii);
-      newAscii= (resultAscii + 65 - Number(offset)) % 26 + 65;
-      resultnew = String.fromCharCode(newAscii);
-      newTermino+=resultnew;
-     
+      if(resultAscii===32){
+        newTermino+=' ';
+      }else{
+        newAscii= (resultAscii + 65 - Number(offset)) % 26 + 65;
+        resultnew = String.fromCharCode(newAscii);
+        newTermino+=resultnew;
+       
+      }
+      
     }
     console.log(newTermino);
-    //return newTermino;
-    document.getElementById("termino").value="palabra cifrada: " + newTermino;
+    return newTermino;
+    
 
     /* los elementos que queremos desplazar a la izquierda */
   }
